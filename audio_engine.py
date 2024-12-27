1427cdcb7fbf4a7a0457bd31e8cf1e826f2fdd17
Ron Urbach <sharpblade4@gmail.com>
2024-12-27 15:43:54 +0200

    fully functioning parrotbear game

--- audio_engine.py
+++ audio_engine.py
@@ -1,40 +1,40 @@
 import pyaudio
 import pyrubberband
 import numpy as np
-import time
-from typing import ByteString
+from typing import ByteString, Callable
 
-CHUNK = 1024
+CHUNK = 2048
 PYAUDIO_FORMAT = pyaudio.paInt16
 NP_FORMAT = np.int16
 CHANNELS = 1
 RATE = 44100
 THRESHOLD = 1000
-SILENCE_DURATION = 2
+SILENCE_DURATION = 1
+SILENCE_COUNTER_THRESHOLD = int(RATE / CHUNK * SILENCE_DURATION)
+VOLUME_INCREASE = 1.2
 
 
 class AudioEngine:
     def __init__(self) -> None:
         self._audio = pyaudio.PyAudio()
-        self._in_stream = self._audio.open(format=PYAUDIO_FORMAT,
-                                           channels=CHANNELS,
-                                           rate=RATE,
-                                           input=True,
-                                           frames_per_buffer=CHUNK,
-                                           input_device_index=None)
-        self._out_stream = self._audio.open(format=PYAUDIO_FORMAT,
-                                            channels=CHANNELS,
-                                            rate=RATE,
-                                            output=True)
+        self._in_stream = self._audio.open(
+            format=PYAUDIO_FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK,
+            input_device_index=None,
+            stream_callback=None,
+            start=False,
+        )
+        self._out_stream = self._audio.open(
+            format=PYAUDIO_FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            output=True,
+            start=False,
+        )
         self._is_loaded: bool = True
-
-    def terminate(self) -> None:
-        self._in_stream.stop_stream()
-        self._out_stream.stop_stream()
-        self._in_stream.close()
-        self._out_stream.close()
-        self._audio.terminate()
-        self._is_loaded = False
 
     def record_audio(self) -> ByteString:
         if not self._is_loaded:
@@ -43,93 +43,100 @@ class AudioEngine:
         self._in_stream.start_stream()
         frames = []
         silent_counter = 0
-        while True:
-            data = self._in_stream.read(CHUNK)
-            frames.append(data)
-            audio_data = np.frombuffer(data, dtype=NP_FORMAT)
-            if np.max(np.abs(audio_data)) < THRESHOLD:
-                silent_counter += 1
-            else:
-                silent_counter = 0
-            if silent_counter >= int(RATE / CHUNK * SILENCE_DURATION):
-                break
+        audio_buffer = np.zeros(CHUNK, dtype=NP_FORMAT)
+
+        try:
+            while True:
+                data = self._in_stream.read(CHUNK, exception_on_overflow=False)
+                frames.append(data)
+
+                np.copyto(audio_buffer, np.frombuffer(data, dtype=NP_FORMAT))
+                audio_level = np.max(np.abs(audio_buffer))
+                if audio_level < THRESHOLD:
+                    silent_counter += 1
+                else:
+                    silent_counter = 0
+
+                if silent_counter >= SILENCE_COUNTER_THRESHOLD:
+                    break
+        except Exception as e:
+            self._in_stream.stop_stream()
+            raise RuntimeError(f"Error during recording: {str(e)}")
+
         self._in_stream.stop_stream()
+        return b"".join(frames)
 
-        audio_data = b''.join(frames)
-        return audio_data
-
-    def play_audio(self, audio_data: ByteString) -> None:
+    def play_audio(self, audio_data: ByteString, complete_callback: Callable) -> None:
+        print("engine playing audio", flush=True)
         if not self._is_loaded:
             raise RuntimeError("AudioEngine terminated")
-        # self._out_stream.start_stream()
+
+        self._out_stream.start_stream()
         self._out_stream.write(audio_data)
-        # self._out_stream.stop_stream()
+        self._out_stream.stop_stream()
 
-    @staticmethod
-    def change_pitch(audio_data: ByteString, factor: float = 2.0) -> ByteString:
-        audio_array = np.frombuffer(audio_data, dtype=NP_FORMAT)
-        shifted_audio = pyrubberband.pitch_shift(audio_array, RATE, factor)
-        # rubberband returned data is represented in float
-        shifted_audio -= shifted_audio.min()
-        shifted_audio /= shifted_audio.max() if shifted_audio.max() != 0 else 1
-        shifted_audio *= (audio_array.max() - audio_array.min())
-        shifted_audio -= audio_array.min()
-        return shifted_audio.astype(np.int16).tobytes()
+        while self._out_stream.is_active():
+            import time
+
+            time.sleep(0.1)
+        complete_callback()
+
+    def change_pitch(self, audio_data: ByteString, factor: float = 2.0) -> ByteString:
+        print("DEBUG: changing pitch2 with factor", factor, flush=True)
+        try:
+            audio_array = np.frombuffer(audio_data, dtype=NP_FORMAT).astype(np.float32)
+            audio_array = audio_array / 32768.0  # normalize 16-bit audio
+            shifted_audio = pyrubberband.pitch_shift(
+                audio_array,
+                RATE,
+                factor,
+            )
+            shifted_audio = np.clip(
+                shifted_audio * 32768.0 * VOLUME_INCREASE, -32768, 32767
+            )
+            return shifted_audio.astype(np.int16).tobytes()
+
+        except Exception as e:
+            print(f"Error during pitch shifting: {e}")
+            return audio_data
+
+    def __del__(self):
+        if hasattr(self, "_in_stream") and self._in_stream:
+            self._in_stream.stop_stream()
+            self._in_stream.close()
+            self._in_stream = None
+        if hasattr(self, "_out_stream") and self._out_stream:
+            self._out_stream.stop_stream()
+            self._out_stream.close()
+            self._out_stream = None
+        if hasattr(self, "_audio") and self._audio:
+            self._audio.terminate()
+            self._audio = None
+        self._is_loaded = False
 
 
-def parrot_game():
-    print("game begins")
-    engine = AudioEngine()
+class AudioEngineMock:
+    def __init__(self) -> None:
+        pass
 
-    silent_counter = 0
-    while True:
-        print("recording")
-        audio_data = engine.record_audio()
-        print(len(audio_data))
-        if silent_counter > 1:
-            break
-        if len(audio_data) <= 176128:
-            print("No audio input. Please try again.")
-            silent_counter += 1
-            continue
-        else:
-            silent_counter = 0
+    def record_audio(self) -> ByteString:
+        print("mock recodring_audio")
+        import time
 
-        parrot_voice = float(np.random.randint(6, 15))
-        high_pitch_audio = engine.change_pitch(audio_data, parrot_voice)
+        time.sleep(2)
+        frames = []
+        for i in range(0, 100):
+            frames.append(
+                np.random.randint(-32768, 32767, CHUNK, dtype=NP_FORMAT).tobytes()
+            )
+        return b"".join(frames)
 
-        engine.play_audio(high_pitch_audio)
+    def play_audio(self, audio_data: ByteString) -> None:
+        print("mock play_audio")
+        import time
 
-        time.sleep(0.3)
+        time.sleep(3)
 
-    engine.terminate()
-
-
-if __name__ == "__main__":
-    parrot_game()
-
-    # info = audio.get_host_api_info_by_index(0)
-    # numdevices = info.get('deviceCount')
-
-    # for i in range(0, numdevices):
-    #     if (audio.get_device_info_by_host_api_device_index(
-    #         0, i).get('maxInputChannels')) > 0:
-    #         print("Input Device id ",
-    #             i,
-    #             " - ",
-    #             audio.get_device_info_by_host_api_device_index(
-    #                 0, i).get('name'))
-
-    # str_input_i = input("choose input source (num):")
-    # try:
-    #     input_i = int(str_input_i)
-    # except ValueError:
-    #     input_i = None
-
-    # input_i = None
-    # stream = audio.open(format=FORMAT,
-    #                     channels=CHANNELS,
-    #                     rate=RATE,
-    #                     input=True,
-    #                     frames_per_buffer=CHUNK,
-    #                     input_device_index=input_i)
+    def change_pitch2(self, audio_data: ByteString, factor: float = 2.0) -> ByteString:
+        print("mock change_pitch")
+        return audio_data
---
+++ gui.py
@@ -0,0 +1,191 @@
+import threading
+import tkinter as tk
+from PIL import Image, ImageTk
+from queue import Queue, Empty
+
+# from audio_engine import AudioEngineMock as AudioEngine
+from audio_engine import AudioEngine
+import numpy as np
+
+
+FIXED_SQUARE_WINDOW = 768
+START_REC_MESSAGE = "Start"
+FREQ_RANGE_BY_ANIMAL = {
+    "parrot": (7, 14),
+    "bear": (-7, -14),
+}
+
+
+class AnimalGui:
+    def __init__(self, master, animal_name: str):
+        self.master = master
+        self.animal_name = animal_name
+        self.current_image_index = 0
+        self.other_animal_window = None
+
+        try:
+            self.talking_animal = Image.open(f"talking_{animal_name}.png")
+            self.looking_animal = Image.open(f"looking_{animal_name}.png")
+        except Exception as e:
+            raise RuntimeError(f"Error loading {animal_name} images:", e)
+            if self.talking_animal:
+                self.talking_animal.close()
+            if self.looking_animal:
+                self.looking_animal.close()
+
+        self.talking_animal = self.talking_animal.resize(
+            (FIXED_SQUARE_WINDOW, FIXED_SQUARE_WINDOW), Image.Resampling.LANCZOS
+        )
+        self.looking_animal = self.looking_animal.resize(
+            (FIXED_SQUARE_WINDOW, FIXED_SQUARE_WINDOW), Image.Resampling.LANCZOS
+        )
+
+        self.talking_photo = ImageTk.PhotoImage(self.talking_animal)
+        self.looking_photo = ImageTk.PhotoImage(self.looking_animal)
+
+        self.current_photo = self.looking_photo
+        self._is_talking = False
+        self.engine = AudioEngine()
+        self.is_processing = False
+        self.auto_restart = False
+
+        self.update_queue = Queue()
+
+        self.master.title(f"ParrotBear: {animal_name.title()}")
+        self.master.resizable(False, False)
+
+        self.canvas = tk.Canvas(
+            master,
+            width=FIXED_SQUARE_WINDOW,
+            height=FIXED_SQUARE_WINDOW,
+            highlightthickness=0,
+        )
+        self.canvas.pack()
+
+        self.image_on_canvas = self.canvas.create_image(
+            0, 0, anchor=tk.NW, image=self.current_photo
+        )
+
+        self.start_button = tk.Button(
+            master, text=START_REC_MESSAGE, command=self.toggle_recording
+        )
+        self.start_button.pack(pady=10)
+
+        self.status_label = tk.Label(master, text="Press the button to start")
+        self.status_label.pack(pady=5)
+
+        self.process_queue()
+
+    def __del__(self):
+        self.talking_animal.close()
+        self.looking_animal.close()
+
+    def toggle_recording(self):
+        if self.auto_restart:
+            self.stop_recording()
+            if self.other_window:
+                self.other_window.start_button.config(state=tk.NORMAL)
+        else:
+            self.start_recording()
+            if self.other_window:
+                self.other_window.start_button.config(state=tk.DISABLED)
+
+    def switch_image(self, is_talking):
+        if self._is_talking == is_talking:
+            return
+
+        self._is_talking = is_talking
+        self.current_photo = self.talking_photo if is_talking else self.looking_photo
+        self.canvas.itemconfig(self.image_on_canvas, image=self.current_photo)
+
+    def start_recording(self):
+        if self.is_processing:
+            print("WARN: Already processing", flush=True)
+            return
+
+        self.start_button.config(text="Stop", state=tk.NORMAL)
+        self.auto_restart = True
+        self.start_recording_thread()
+
+    def start_recording_thread(self):
+        thread = threading.Thread(target=self.process_audio, daemon=True)
+        thread.start()
+
+    def process_audio(self):
+        self.is_processing = True
+
+        try:
+            self.update_queue.put((False, "Recording. . . ..."))
+            audio_data = self.engine.record_audio()
+            pitched_voice = float(
+                np.random.uniform(*FREQ_RANGE_BY_ANIMAL[self.animal_name])
+            )
+            self.update_queue.put((True, "Playing..."))
+            high_pitch_audio = self.engine.change_pitch(audio_data, pitched_voice)
+
+            def on_playback_complete():
+                if self.auto_restart:
+                    self.update_queue.put(("restart", None))
+                else:
+                    self.update_queue.put((False, "Ready"))
+                    self.update_queue.put(("button", START_REC_MESSAGE))
+
+            self.engine.play_audio(high_pitch_audio, on_playback_complete)
+
+        except Exception as e:
+            self.update_queue.put((False, f"Error: {str(e)}"))
+            self.auto_restart = False
+            self.is_processing = False
+            self.update_queue.put(("button", START_REC_MESSAGE))
+            # self.update_queue.put((False, "Ready"))
+        finally:
+            self.is_processing = False
+
+    def stop_recording(self):
+        self.auto_restart = False
+        self.start_button.config(text=START_REC_MESSAGE, state=tk.NORMAL)
+        self.status_label.config(text="Stopped")
+
+    def process_queue(self):
+        try:
+            while True:
+                item = self.update_queue.get_nowait()
+                if isinstance(item, tuple):
+                    # print("DEBUG: got event:" , item)
+                    message_type, status_text = item
+                    if message_type == "button":
+                        self.start_button.config(text=status_text, state=tk.NORMAL)
+                    if message_type == "restart":
+                        self.start_recording_thread()
+                    else:
+                        self.status_label.config(text=status_text)
+                        self.switch_image(message_type)
+                else:
+                    print("WARN got unexpected event:", item)
+        except Empty:
+            pass
+        finally:
+            # Schedule the next queue processing
+            self.master.after(100, self.process_queue)
+
+
+def main():
+    root = tk.Tk()
+    second_window = tk.Toplevel()
+
+    root.geometry(f"{FIXED_SQUARE_WINDOW}x{FIXED_SQUARE_WINDOW + 100}+50+50")
+    second_window.geometry(
+        f"{FIXED_SQUARE_WINDOW}x{FIXED_SQUARE_WINDOW + 100}+{FIXED_SQUARE_WINDOW + 100}+50"
+    )
+
+    parrot_app = AnimalGui(root, "parrot")
+    bear_app = AnimalGui(second_window, "bear")
+
+    parrot_app.other_window = bear_app
+    bear_app.other_window = parrot_app
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
